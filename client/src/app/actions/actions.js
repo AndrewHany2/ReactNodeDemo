8703247e5dce4ba01f07e4578b0d097589b39c97
Andrew Hany <andrewhany260@gmail.com>
2021-07-01 20:16:20 +0200

    a

--- client/src/app/actions/actions.js
+++ client/src/app/actions/actions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = "http://localhost:8000/api/users";
+const baseURL = "/api/users";
 
 export const filterUsersByName = (name) => async (dispatch, getState) => {
   try {
--- index.js
+++ index.js
@@ -2,11 +2,9 @@ require("./helpers/dbConnection");
 const express = require("express");
 const morgan = require("morgan");
 const userRouter = require("./routers/userRouter.js");
-var cors = require("cors");
 const app = express();
-const port = 8000;
+const PORT = process.env.PORT || 8000;
 
-app.use(cors());
 app.use(morgan("dev"));
 app.use("/uploads", express.static("uploads"));
 app.use(express.json());
@@ -14,15 +12,14 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/users", userRouter);
 app.use((err, req, res, next) => {
-  console.log(err);
   res.status(500).json({ msg: err });
 });
-const path = require("path");
-app.get("/", (req, res) => {
-  app.use(express.static(path.resolve(__dirname, "client", "build")));
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+const root = require("path").join(__dirname, "client", "build");
+app.use(express.static(root));
+app.get("*", (req, res) => {
+  res.sendFile("index.html", { root });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
